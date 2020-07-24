75125b9b0fc47ff2ba1ce0f54e1e246e430dd4e7
fefuzz <pierpaolovanni@gmail.com>
2020-07-24 18:01:09 +0200

    Updates

---
+++ precache-manifest.1f2ce39f297f7268b426e1c0e556359e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "54c77d8acca4cb049468f793d5b7221c",
+    "url": "/myPortfolio/index.html"
+  },
+  {
+    "revision": "0d39190792c9d982b760",
+    "url": "/myPortfolio/static/css/main.9aac0dc9.chunk.css"
+  },
+  {
+    "revision": "b82ba235b0668cdeff97",
+    "url": "/myPortfolio/static/js/2.8e885b32.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/myPortfolio/static/js/2.8e885b32.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "0d39190792c9d982b760",
+    "url": "/myPortfolio/static/js/main.78f9d174.chunk.js"
+  },
+  {
+    "revision": "f7e2d6d97b522e9ec6a3",
+    "url": "/myPortfolio/static/js/runtime-main.9164ea68.js"
+  }
+]);
